2c1c855fa0310d2e314d11f38cf31438f05a9ac2
Sadra <ti9mail@gmail.com>
2018-03-28 11:11:28 +0200

    add project structure.

---
+++ src/main/java/de/sadrab/Mp3PiratesoftheCodeBean1TheCursedCoin/Mp3PiratesOfTheCodeBean1TheCursedCoinApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp3PiratesoftheCodeBean1TheCursedCoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3PiratesOfTheCodeBean1TheCursedCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3PiratesOfTheCodeBean1TheCursedCoinApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sadrab/Mp3PiratesoftheCodeBean1TheCursedCoin/Mp3PiratesOfTheCodeBean1TheCursedCoinApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sadrab.Mp3PiratesoftheCodeBean1TheCursedCoin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp3PiratesOfTheCodeBean1TheCursedCoinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
